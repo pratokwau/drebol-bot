c7ee5e4ec29493b3490f75729d1fb58627c6eb3a
Maxim <pratokwau@gmail.com>
2026-06-25 02:20:41 +0300

    Delete states directory

--- states/playerokrass_states.py
+++
@@ -1,7 +0,0 @@
-# states/playerokrass_states.py
-from aiogram.fsm.state import State, StatesGroup
-
-class PlayerOkStates(StatesGroup):
-    waiting_sale_commission = State()
-    waiting_withdraw_commission = State()
-    waiting_prices = State()
